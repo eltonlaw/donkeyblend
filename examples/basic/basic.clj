982c4780fdff7967b54e73aa9ee30f37a5f7bafe
Elton Law <elton.law@kirasystems.com>
2019-09-02 23:31:24 -0400

    update example

--- examples/basic/basic.clj
+++ examples/basic/basic.clj
@@ -4,10 +4,18 @@
             [donkeyblend.python :as py]
             [donkeyblend.io :as db.io]))
 
-(defscript create_square
+(defscript init
   (py/import "bpy")
+  (py/import "mathutils")
+  "\n"
+  (py/assign "C" "bpy.context")
+  (py/assign "D" "bpy.data"))
+
+(defscript create_square
+  init
   (py/set-bl-info {:name "some script"
                    :description "does something"})
+  "\n"
   (py/print "hello")
   (py/assign "context_name" (py/attr :bpy :context :object :name))
   (py/assign "context" (py/attr :bpy :data :objects [:get :context_name])))
