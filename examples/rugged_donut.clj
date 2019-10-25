11c883a19aad2155ed758d34bf2eb578fa57769d
Elton Law <eltonlaw296@gmail.com>
2019-10-24 23:16:07 -0400

    example - import python modules
    This clojure example is equivalent to:
    
        import bpy
        from math import pi sin cos
        import utils
        import os
    
    It's a pretty straightforward translation, though I did debate on the
    syntax, Clojure has three ways of pulling in external code `require`,
    `import` and `use`. Here's what I can tell:
    
    - `use`... has been deprecated since 1.4.0
    - `require`...is used for Clojure libraries
    - `import`...is used for interop with host VM libraries (Java for clj,
    ClojureScript for cljs)
    
    With this and no seemingly important reason to introduce new
    syntax/structure, `import` seems like the clear choice.
    
    Imports follow the general structure of Python imports, stdlib first
    then external libs then custom ones.
    
    TODO: How, if possible can we add linting to check that libs are
    installed?

--- examples/rugged_donut.clj
+++ examples/rugged_donut.clj
@@ -1,4 +1,7 @@
 (ns rugged-donut
   ;; https://github.com/njanakiev/blender-scripting/blob/master/scripts/rugged_donut.py 
   ;; An attempt at forcing the above python script into idiomatic clojure
-  (:require [donkeyblend.core :as db]))
+  (:import os
+           (math pi sin cos)
+           bpy
+           utils))
