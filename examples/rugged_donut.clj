88e1e6e427308c345bf029e0afee4b44ba6c5589
Elton Law <eltonlaw296@gmail.com>
2019-10-24 23:38:16 -0400

    example - global vars + calling custom functions
    This covers lines 6-13. the initialization code of the script, it's a
    pretty 1-1 mapping from the clojure code to python code
    
    The import on `utils` was moved to a require as it seems more likely
    thatcustom scripts will also be written with donkeyblend (most likely?),
    aka a clojure script, thus using `require` instead. Though using
    `import` still works (for custom scripts written in Python).

--- examples/rugged_donut.clj
+++ examples/rugged_donut.clj
@@ -3,5 +3,14 @@
   ;; An attempt at forcing the above python script into idiomatic clojure
   (:import os
            (math pi sin cos)
-           bpy
-           utils))
+           bpy)
+  (:require [utils :as u]))
+
+(def num-frames 100)
+
+(defn -main []
+  (utils/remove-all)
+  (let [[target camera lamp] (u/simple-scene [0 0 -3]
+                                             [4.2 4.2 5]
+                                             [-5 5 10])]
+    (println "...")))
