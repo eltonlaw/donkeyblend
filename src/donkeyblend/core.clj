9d7899a0dce6e90820580c4c2397df013e2969b2
Elton Law <eltonlaw296@gmail.com>
2019-10-26 11:44:25 -0400

    add reader

--- src/donkeyblend/core.clj
+++ src/donkeyblend/core.clj
@@ -1,1 +1,8 @@
-(ns donkeyblend.core)
+(ns donkeyblend.core
+  (:require [donkeyblend.reader :as reader]))
+
+(defn -main [& args]
+  "Passing in a file"
+  (let [[file & _] args]
+    (-> (slurp file)
+        reader/read-string)))
