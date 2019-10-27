21af773ac52b9cc10c42d3ef8e4ca048b2234088
Elton Law <elton.law@kirasystems.com>
2019-10-26 23:08:24 -0400

    reader option initialization
    Skeleton code for the reader options. A little bit about the opts:
    
    - `:readers` is passing in functions to read custom symbols
    - `:default` is the default case for if none of the readers match. Not
    too sure how this works yet, the documentation in `cljs.reader/read`
    seems to indicate that it's a function of two arguments `tag` and
    `value`...not too sure what `value` is supposed to be yet. Something to
    keep in mind
    -  `:eof` is the value that gets returned for an EOF character. `nil` is
    already set as default but looking at the implementation, it needs to be
    explicitly passed if a map is being passed in as the first argument.
    
    Tests still pass

--- src/donkeyblend/reader.clj
+++ src/donkeyblend/reader.clj
@@ -1,6 +1,21 @@
 (ns donkeyblend.reader
-  (:refer-clojure :exclude [read-string read])
+  (:refer-clojure :exclude [read-string read *default-data-reader-fn*])
   (:require [clojure.tools.reader.edn :as edn]))
 
-(defn read-string [s]
-  (edn/read-string (str \( s \))))
+;; default fn for handling tags without a reader
+(def ^:dynamic *default-data-reader-fn*
+  (atom (constantly :unknown-tag)))
+;; map with symbol (... our custom tag) as key and fn as value
+(def ^:dynamic *tag-table*
+  (atom {}))
+
+(defn read-string
+  "Given a string return EDN formatted data.
+
+  NOTE: This returns a list of ALL objects in the string"
+  [s]
+  (edn/read-string
+     {:readers @*tag-table*
+      :default @*default-data-reader-fn*
+      :eof nil}
+     (str \( s \))))
