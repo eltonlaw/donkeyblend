4e839bb04e0fa2a44be6ca5251b3e585ed15e17d
Elton Law <elton.law@kirasystems.com>
2019-10-27 03:14:55 -0400

    analyzer - flesh out higher level API
    `tools.analyzer` is a framework for writing a custom analyzer/compiler.
    
    To use it the following logic needs to be specified: macroexpansion,
    variable creation, parsing of forms, what a variable is, how the global
    env works and what optimization passes will run on the ast.

--- src/donkeyblend/tools/analyzer/py.clj
+++ src/donkeyblend/tools/analyzer/py.clj
@@ -1,1 +1,37 @@
-(ns donkeyblend.tools.analyzer.py)
+(ns donkeyblend.tools.analyzer.py
+  "Analyzer for clojure code, extends tools.analyzer with Python specific passes/forms
+  **focus is on blender's python interface**"
+  (:refer-clojure :exclude [var? macroexpand-1 macroexpand])
+  (:require [clojure.tools.analyzer
+             :as ana
+             :refer [analyze analyze-in-env wrapping-meta analyze-fn-method]
+             :rename {analyze -analyze}]
+            [clojure.tools.analyzer
+             [env :as env :refer [*env*]]]))
+
+(defn macroexpand-1 [form env] nil)
+
+(defn parse [[op & args] env] nil)
+
+(defn create-var [sym env] nil)
+
+(defn var? [obj] nil)
+
+(defn empty-env [] {})
+
+(defn global-env [] (atom {}))
+
+(defn run-passes [ast] ast)
+
+(defn analyze
+  "Analyzes a clojure form using tools.analyzer augmented with
+  the Python specific special ops and returns an AST"
+  ([form] (analyze form (empty-env) {}))
+  ([form env] (analyze form env {}))
+  ([form env opts]
+   (binding [ana/macroexpand-1 macroexpand-1
+             ana/create-var    create-var
+             ana/parse         parse
+             ana/var?          var?]
+     (env/ensure (global-env)
+       (run-passes (-analyze form env))))))
---
+++ test/donkeyblend/tools/analyzer/py_test.clj
@@ -0,0 +1,3 @@
+(ns donkeyblend.tools.analyzer.py-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [donkeyblend.tools.analyzer.py :as ana.py]))
