8402eaf5bfdceca2764e63c924c45fdaff571ffe
Elton Law <eltonlaw296@gmail.com>
2020-07-19 17:38:34 -0400

    emit constants

---
+++ test/donkeyblend/tools/emitter/py_test.clj
@@ -0,0 +1,16 @@
+(ns donkeyblend.tools.emitter.py-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [donkeyblend.tools.analyzer.py :as analyzer]
+            [donkeyblend.tools.emitter.py :as emitter]
+            [donkeyblend.reader :as reader]))
+
+(defn- to-py [s]
+  (-> (reader/read-string s)
+      analyzer/analyze
+      emitter/emit))
+
+(deftest const-node
+  (is (= "True" (to-py "true")))
+  (is (= "1" (to-py "1")))
+  (is (= "1.5" (to-py "1.5")))
+  (is (= "\"a\"" (to-py ":a"))))
