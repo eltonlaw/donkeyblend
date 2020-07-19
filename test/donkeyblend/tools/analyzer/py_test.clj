45988a6334a35489e5ab27bff976c944fa0b823e
Elton Law <eltonlaw296@gmail.com>
2020-07-19 18:14:05 -0400

    add tests for analyzing if

--- test/donkeyblend/tools/analyzer/py_test.clj
+++ test/donkeyblend/tools/analyzer/py_test.clj
@@ -1,18 +1,31 @@
 (ns donkeyblend.tools.analyzer.py-test
-  (:require [clojure.test :refer [is deftest testing]]
-            [donkeyblend.reader :as reader]
-            [donkeyblend.tools.analyzer.py :as ana.py]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [clojure.tools.analyzer.ast :as ast]
+            [donkeyblend.tools.analyzer.py :as analyzer]
+            [donkeyblend.tools.emitter.py :as emitter]
+            [donkeyblend.reader :as reader]))
 
 (defn- dissoc-in [m ks]
   (update-in m (butlast ks) dissoc (last ks)))
 
-(deftest analyze-test
-  (testing "Number literal"
-    (is (= {:op :const
-            :env {:context :ctx/expr :locals {}}
-            :type :number
-            :literal? true
-            :val 1
-            :form 1
-            :top-level true}
-           (dissoc-in (ana.py/analyze '1) [:env :ns])))))
+(defn- analyze [s]
+  (analyzer/analyze (reader/read-string s)))
+
+(deftest analyze-number-literals
+  (let [result (analyze "1")]
+    (is (= :const (:op result)))
+    (is (= :number (:type result)))
+    (is (= 1 (:val result)))))
+
+(deftest analyze-arithmetic
+  (let [result (analyze "(+ 1 2)")]
+    (is (= :invoke (:op result)))
+    (is (= #'clojure.core/+ (-> result :fn :var)))))
+
+(deftest analyze-if
+  (let [result (analyze "(if true :then :else)")
+        children (ast/children result)]
+    (is (= :if (:op result)))
+    (is (= true (:val (first children))))
+    (is (= :then (:val (second children))))
+    (is (= :else (:val (nth children 2))))))
