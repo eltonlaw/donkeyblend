de7cd31cd8c922265be607ef83b873fdcca0d271
Elton Law <eltonlaw296@gmail.com>
2020-07-19 17:53:01 -0400

    setup main function to run full process

--- src/donkeyblend/core.clj
+++ src/donkeyblend/core.clj
@@ -1,8 +1,17 @@
 (ns donkeyblend.core
-  (:require [donkeyblend.reader :as reader]))
+  (:require [clojure.string :as str]
+            [donkeyblend.reader :as reader]
+            [donkeyblend.tools.analyzer.py :as analyzer]
+            [donkeyblend.tools.emitter.py :as emitter]))
 
-(defn -main [& args]
-  "Passing in a file"
-  (let [[file & _] args]
-    (-> (slurp file)
-        reader/read-string)))
+(defn -main
+  "Reads in clojure file, runs reader, analyzer and emitter saving output to 
+  input file with a `.py` extension"
+  [& args]
+  (let [[path & _] args
+        path-out (str/replace path #".clj" ".py")
+        raw-text (slurp path)
+        forms (reader/read-string raw-text)
+        ast (analyzer/analyze forms)
+        py (emitter/emit ast)]
+    (spit path-out py)))
