fc3d2d51ba4f300f0c32d62c6b4ed83b9af18751
Elton Law <eltonlaw296@gmail.com>
2020-07-19 18:10:30 -0400

    update rugged_donut example

--- examples/rugged_donut.clj
+++ examples/rugged_donut.clj
@@ -4,23 +4,28 @@
   (:import os
            (math pi sin cos)
            bpy)
-  (:require [utils :as u]))
+  (:require [utils]))
 
+;; Sets number of frame
 (def num-frames 100)
 
+(defn empty-obj
+  (.. bpy (data) (objects) (new "Empty" nil)))
+
 (defn -main []
-  (utils/remove-all)
-  (let [[target camera lamp] (u/simple-scene [0 0 -3]
-                                             [4.2 4.2 5]
-                                             [-5 5 10])
-        empty-obj (-> bpy (.data) (.objects) (.new "Empty", nil))
-        transition-fn #(vec (+ (* 0.7 (cos (* 2 pi %))) 1)
-                            (* 0.7 (sin (* 2 pi %)))
-                            (* 0.4 (sin (* 2 pi %))))]
-    (for [i (range 100)]
+  ;; Remove all elements
+  (utils/remove-all!)
+
+  (let [[target camera lamp] (utils/simple-scene [0 0 -3]
+                                                 [4.2 4.2 5]
+                                                 [-5 5 10])
+        eobj (empty-obj)
+        transition-fn #(vec (+ (* 0.7 (cos (* 2 pi %))) 1
+                               (* 0.7 (sin (* 2 pi %)))
+                               (* 0.4 (sin (* 2 pi %)))))]
+    (doseq [frame (range 100)]
       (.keyframe_insert {^Attribute :location [x y z]
                          :data_path "locations"
                          :index -1
                          :frame frame}
-                        (/ i num-fames)
-                        (transition-fn t)))))
+                        (transition-fn (/ frame num-frames))))))
