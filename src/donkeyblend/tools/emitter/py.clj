563812a80c63d2ccfa4bb5f203bc63814795f922
Elton Law <eltonlaw296@gmail.com>
2020-07-20 01:28:05 -0400

    modify add to handle other arithmetic ops

--- src/donkeyblend/tools/emitter/py.clj
+++ src/donkeyblend/tools/emitter/py.clj
@@ -32,12 +32,18 @@
               :invoke (from-x-import-y ast)))]
     (str/join "\n" (map dispatch args))))
 
+(defn -emit-arithmetic [operator ast]
+  (let [args-py-str (->> (ana.ast/children ast)
+                         rest
+                         (map -emit))]
+    (->> args-py-str
+         (str/join operator)
+         (format "(%s)"))))
+
 (defmethod -emit :invoke
   [{:keys [fn] :as ast}]
-  (println "INVOKE" (keys ast))
   (let [fn-name (name (get-in fn [:meta :name]))]
     (case fn-name
       "import" (-emit-invoke-import ast)
-      ;; FIXME
-      "+" (str/join "+" (map -emit (rest (ana.ast/children ast))))
+      ("+" "-" "*" "/")  (-emit-arithmetic fn-name ast)
       (throw (Exception. (format "Emitting :invoke - unknown var %s" fn-name))))))
