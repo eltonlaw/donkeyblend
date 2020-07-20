43d8f00cfeed8ebd4faa95f516d608810c405f3f
Elton Law <eltonlaw296@gmail.com>
2020-07-20 00:55:35 -0400

    emit imports

--- src/donkeyblend/tools/emitter/py.clj
+++ src/donkeyblend/tools/emitter/py.clj
@@ -14,4 +14,26 @@
     :bool (str/capitalize val)
     :keyword (str "\"" (name val) "\"")
     :number (str val)
-    (throw (Exception. (format "Unknown type %s with value %s" type val)))))
+    (throw (Exception. (format "Emitting :const - unknown type %s with value %s" type val)))))
+
+(defn -emit-invoke-import
+  "Can't handle the deeply nested structure that clojure imports enable, can at most only
+  do one layer of importing from a module"
+  [{:keys [args]}]
+  (letfn [(from-x-import-y [ast]
+            (let [[x & ys] (:form ast)]
+              (format "from %s import %s" x (str/join "," ys))))
+          (import-x [ast]
+            (str "import " (:class ast)))
+          (dispatch [ast]
+            (case (:op ast)
+              :maybe-class (import-x ast)
+              :invoke (from-x-import-y ast)))]
+    (str/join "\n" (map dispatch args))))
+
+(defmethod -emit :invoke
+  [{:keys [fn] :as ast}]
+  (let [fn-name (name (get-in fn [:meta :name]))]
+    (case fn-name
+      "import" (-emit-invoke-import ast)
+      (throw (Exception. (format "Emitting :invoke - unknown var %s" fn-name))))))
--- test/donkeyblend/tools/emitter/py_test.clj
+++ test/donkeyblend/tools/emitter/py_test.clj
@@ -9,8 +9,16 @@
       analyzer/analyze
       emitter/emit))
 
-(deftest const-node
+(deftest emit-const
   (is (= "True" (to-py "true")))
   (is (= "1" (to-py "1")))
   (is (= "1.5" (to-py "1.5")))
   (is (= "\"a\"" (to-py ":a"))))
+
+(deftest emit-invoke-import
+  (is (= "import bpy"
+         (to-py "(import bpy)")))
+  (is (= "import bpy\nimport os"
+         (to-py "(import bpy os)")))
+  (is (= "import bpy\nimport os\nfrom math import pi,sin,cos"
+         (to-py "(import bpy os (math pi sin cos))"))))
